ebab903dec193ba4067d38c66cf2e18347a88b8e
Stone C. Lasley <stonelasley@gmail.com>
2021-12-18 07:39:57 -0700

    feat(backlight): trying to disable keymaps

--- keyboards/keebio/sinc/rules.mk
+++ keyboards/keebio/sinc/rules.mk
@@ -20,7 +20,7 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
@@ -29,5 +29,6 @@ RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 LTO_ENABLE = yes
 SPLIT_KEYBOARD = yes
+ENCODER_ENABLE = yes
 
 DEFAULT_FOLDER = keebio/sinc/rev1
